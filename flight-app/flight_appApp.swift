b85764f3bf12edb58b9f60462a77685ba4036a29
Ivanitskii Artem <dalantren@gmail.com>
2025-04-02 22:49:53 +0200

    Initial Commit

---
+++ flight-app/flight_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  flight_appApp.swift
+//  flight-app
+//
+//  Created by Artem Ivanitskii on 02/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct flight_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
